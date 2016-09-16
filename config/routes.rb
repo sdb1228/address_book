53458681b1e8f3a1af8a45cbec2d895753a394d4
steven burnett <stevendburnett@gmail.com>
2016-09-16 17:16:49 -0600

    map contacts resource to rot

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+  resources :contacts, :path => '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
